1f584cf2a7724371be8aa31a7d2a762a07637456
Nicholas <nicholas.ackley56@gmail.com>
2026-03-05 13:23:58 -0600

    Fixed Collisions and successfully object drops items when reached maxDamage. New Bug: Tree animations are slowed and delayed.

--- skybloom-game/scenes/object/tree/small_tree.gd
+++ skybloom-game/scenes/object/tree/small_tree.gd
@@ -3,6 +3,8 @@ extends Sprite2D
 @onready var damage_component: DamageComponent = $DamageComponent
 @onready var hurt_component: HurtComponent = $HurtComponent
 
+var log_scene = preload("res://scenes/object/tree/log.tscn")
+
 func _ready() -> void:
 	hurt_component.hurt.connect(on_hurt)
 	damage_component.max_damaged_reached.connect(on_max_damage_reached)
@@ -11,5 +13,12 @@ func on_hurt(hit_damage: int) -> void:
 	damage_component.apply_damage(hit_damage)
 
 func on_max_damage_reached() -> void:
+	call_deferred("add_log_scene")
 	print("max damage reached")
 	queue_free()
+	
+func add_log_scene() -> void:
+	var log_instance = log_scene.instantiate() as Node2D
+	log_instance.global_position = global_position
+	get_parent().add_child(log_instance)
+	
